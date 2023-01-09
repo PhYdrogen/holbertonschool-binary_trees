103619fa6b82810e5cabffb8a1043817a53f98f4
Gabriel <fossecruor@gmail.com>
2023-01-09 04:39:42 -0800

    added comment

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -10,36 +10,40 @@ void binary_tree_delete(binary_tree_t *tree)
 
 	if (tree == NULL)
 		return;
-
 	traveler = tree;
 	/* loop in the tree */
+	/* the (traveler &&) is to make sure we don't get segfault */
 	while (tree->right != NULL || tree->left != NULL)
 	{
+		/* if there is left go to then back to the main loop */
 		if (traveler && traveler->left != NULL)
 		{
 			traveler = traveler->left;
-			printf("je suis aller a gauche\n");
 			continue;
 		}
+		/* if there is right go to then back to the main loop */
 		if (traveler && traveler->right != NULL)
 		{
 			traveler = traveler->right;
-			printf("je suis aller a droite\n");
 			continue;
 		}
+		/* if we get here it's because there is nothing left*/
+		/* we check just in case */
 		if (traveler && traveler->left == NULL && traveler->right == NULL)
 		{
-			printf("je viens de free\n");
-
+			/* so we don't really know who we are, left or right */
+			/* but we can check the adresses, that's what we do */
+			/* and we set the parent to null so we don't double free */
 			if (traveler->parent->right == traveler)
 				traveler->parent->right = NULL;
 			else
 				traveler->parent->left = NULL;
-
+			/* we free like normal*/
 			free(traveler);
 			traveler = NULL;
 			traveler = tree;
 		}
 	}
+	/* we free the root*/
 	free(tree);
 }
