7b52b18d448c402ed04096f34c84e7cce37a81bc
Gabriel <fossecruor@gmail.com>
2023-01-09 01:16:59 -0800

    Adding prototype for header file

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - function that creates a binary tree node
+ * @parent: the ptr of the parent
+ * @value: the value
+ * Return: the node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+}
