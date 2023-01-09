396202d4ea2c30fa9bcd8ec2323b94db935daa71
Gabriel <fossecruor@gmail.com>
2023-01-09 06:29:15 -0800

    adding content for 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - loop in the tree
+ * @tree: the tree
+ * Return: the depth of the tree
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree->parent != NULL)
+	{
+		return (1 + binary_tree_depth(tree->parent));
+	}
+	return (0);
+}
